8e414df5155d0e88d847e0ecf9844ce802eb9547
Zhaksybek <zhaks18121993@gmail.com>
2020-07-22 16:31:37 -0700

    Jack's commit

---
+++ src/main/java/blue/Everyone.java
@@ -0,0 +1,13 @@
+package blue;
+
+public class Everyone {
+    public static void main(String[] args) {
+        System.out.println("Everyone need to create new class");
+
+        // Today was long day;    lol ))
+
+
+
+
+    }
+}
