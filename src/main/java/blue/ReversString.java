0e820897622ca52ac8ded207bdf088f3ac0593c5
Zhaksybek <zhaks18121993@gmail.com>
2020-07-22 21:39:36 -0700

    hhfjff

--- src/main/java/blue/ReversString.java
+++ src/main/java/blue/ReversString.java
@@ -35,6 +35,10 @@ public class ReversString {
         System.out.println("Reversed array is: \n");
         for (int k = 0; k < n; k++) {
             System.out.println(b[k]);
+            System.out.println("dfdf");
+            System.out.println("dfdf");
+            System.out.println("dfdf");
+            System.out.println("dfdf");
         }
     }
 
